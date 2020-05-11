292e86c1caa47123d8e6bdc4c1492796ed76b50e
Vlad_Boltrukanis <boltrukanisvlad@gmail.com>
2020-05-11 19:26:22 +0300

    feat: add FileUtil (read and wite

--- src/main/java/com/solvd/lab/automation/formatter/util/FileUtil.java
+++ src/main/java/com/solvd/lab/automation/formatter/util/FileUtil.java
@@ -1,4 +1,34 @@
 package com.solvd.lab.automation.formatter.util;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class FileUtil {
+
+  public static String getStringFromFile(String fileName){
+    String result = "";
+    try{
+      FileReader reader = new FileReader(fileName);
+      Scanner scan = new Scanner(reader);
+      while(scan.hasNextLine()){
+        result += scan.nextLine() + '\n';
+      }
+    } catch(IOException ex) {
+      System.out.println(ex.getMessage());
+    }
+
+    return result;
+  }
+
+  public static void saveStringInFile(String fileName, String text){
+    try {
+      FileWriter fw = new FileWriter(fileName);
+      fw.write(text);
+      fw.close();
+    } catch(IOException ex) {
+      System.out.println(ex.getMessage());
+    }
+  }
 }
